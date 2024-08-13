dfdca1351779c08fde6fa9ab74bd596762face93
Krzysztof Gierczak <104378746+KrzysztofGier@users.noreply.github.com>
2024-08-13 10:34:03 +0200

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,18 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  // write code here
+  function calculateRentalCost(days) {
+  let result = days * 40;
+
+  if (days >= 7) {
+    result = result - 50;
+  } else if (days >= 3) {
+    result = result - 20;
+  }
+
+  return result;
+}
+module.exports = calculateRentalCost;
 }
 
 module.exports = calculateRentalCost;
